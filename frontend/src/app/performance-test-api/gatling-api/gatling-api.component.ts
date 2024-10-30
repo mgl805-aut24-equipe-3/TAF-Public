b7b516aa0f2fde2dd03278f0c513821f6497d3cb
Alex Nezami <alex.nezamii@gmail.com>
2024-10-30 18:03:21 -0400

    ajouter les validations des champs pour Gatling

--- frontend/src/app/performance-test-api/gatling-api/gatling-api.component.ts
+++ frontend/src/app/performance-test-api/gatling-api/gatling-api.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs';
 import { PerformanceTestApiService } from 'src/app/_services/performance-test-api.service';
 import Swal from 'sweetalert2';
 import { GatlingRequest } from './gatling-request';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-gatling-api',
@@ -27,7 +28,43 @@ export class GatlingApiComponent implements OnInit {
     this.span = document.getElementsByClassName("close")[0] as HTMLElement;
   }
 
+  validateForm(): boolean {
+    let isValid = true;
+    const requiredFields = [
+      { element: 'testScenarioName', errorMessage: 'Veuillez entrer une valeur' },
+      { element: 'testBaseUrl', errorMessage: 'Veuillez entrer une valeur' },
+      { element: 'testUri', errorMessage: 'Veuillez entrer une valeur' },
+      { element: 'testMethodType', errorMessage: 'Veuillez sélectionner un type de requête' },
+      { element: 'userNumber', errorMessage: 'Veuillez entrer ou sélectionner une valeur' }
+    ];
+
+    requiredFields.forEach(field => {
+      const inputElement = document.getElementsByName(field.element)[0] as HTMLInputElement | null;
+      const errorDiv = document.createElement('div');
+      errorDiv.className = 'text-danger';
+
+      if (inputElement?.nextElementSibling) {
+        inputElement.nextElementSibling.remove();
+      }
+      if (inputElement && inputElement.value.trim() === '') {
+        isValid = false;
+        inputElement.classList.add('is-invalid');
+        errorDiv.innerText = field.errorMessage;
+        inputElement.insertAdjacentElement('afterend', errorDiv);
+      } else {
+        inputElement?.classList.remove('is-invalid');
+      }
+    });
+
+    return isValid;
+  }
+
   onSubmit() {
+
+    if (!this.validateForm()) {
+      return;
+    }
+
     this.busy = this.performanceTestApiService.sendGatlingRequest(this.request)
       .subscribe((response: any) => {
         this.modal!.style.display = "block";
