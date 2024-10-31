5394d35be0336fe3165fade15f5d4c858839983c
Julien Champagne <jchampanzee@gmail.com>
2024-10-31 11:35:49 -0400

    Modif du path pour retrouver le jar de gatling avec sees dépendances

--- backend/src/main/java/ca/etsmtl/taf/provider/GatlingJarPathProvider.java
+++ backend/src/main/java/ca/etsmtl/taf/provider/GatlingJarPathProvider.java
@@ -12,7 +12,7 @@ public class GatlingJarPathProvider {
         Path parentDirectory = backendDirectory.getParent();
 
         //Construire le chemin vers le Gatling JAR
-        File gatlingJar = parentDirectory.resolve("gatling").resolve("target").resolve("gatling-1.0-SNAPSHOT-jar-with-dependencies.jar").toFile();
+        File gatlingJar = parentDirectory.resolve("performance").resolve("gatling").resolve("target").resolve("taf-performance-gatling-1.0.0-SNAPSHOT-jar-with-dependencies.jar").toFile();
         if (gatlingJar.exists()) {
             return gatlingJar.getAbsolutePath();
         } else {
