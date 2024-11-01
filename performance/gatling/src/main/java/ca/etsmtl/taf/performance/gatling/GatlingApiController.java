0ded4ebd3de1cf835b493162736baea07353eebb
Julien Champagne <jchampanzee@gmail.com>
2024-11-01 17:41:02 -0400

    Deletion de packages qui ne sont plus utilisés

--- performance/gatling/src/main/java/ca/etsmtl/taf/performance/gatling/GatlingApiController.java
+++ performance/gatling/src/main/java/ca/etsmtl/taf/performance/gatling/GatlingApiController.java
@@ -35,7 +35,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ca.etsmtl.taf.performance.gatling.payload.response.MessageResponse;
 import ca.etsmtl.taf.performance.gatling.entity.GatlingTestRequest;
-import ca.etsmtl.taf.performance.gatling.provider.GatlingJarPathProvider;
 import ca.etsmtl.taf.performance.gatling.config.GatlingConfigurator;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
