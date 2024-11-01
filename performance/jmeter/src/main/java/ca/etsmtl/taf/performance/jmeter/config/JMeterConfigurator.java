f51d933d41ad4c95538609611d4032c82488e8d0
JF Leblay <jflb.mail@gmail.com>
2024-11-01 00:09:49 -0400

    refactor(gatling): removed unused imports

--- performance/jmeter/src/main/java/ca/etsmtl/taf/performance/jmeter/config/JMeterConfigurator.java
+++ performance/jmeter/src/main/java/ca/etsmtl/taf/performance/jmeter/config/JMeterConfigurator.java
@@ -18,7 +18,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
