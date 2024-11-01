8a5f3874d7d971a8deebd18f8ee4c01bc51b245d
Abdel Nasser MAIGA <82781037+Nasser07-git@users.noreply.github.com>
2024-10-31 21:40:12 -0400

    Problème de CORS avec cette methode

--- frontend/src/app/_services/performance-test-api.service.ts
+++ frontend/src/app/_services/performance-test-api.service.ts
@@ -8,7 +8,7 @@ import { JMeterFTPRequest } from '../performance-test-api/jmeter-api/jmeter-ftp-
 import { GatlingRequest } from '../performance-test-api/gatling-api/gatling-request';
 
 const GATLING_API = `${environment.apiUrl}/api/gatling/runSimulation`;
-const LATEST_REPORT_API = `${environment.apiUrl}/api/gatling/latest-report`;
+// const LATEST_REPORT_API = `${environment.apiUrl}/api/gatling/latest-report`;
 const JMeter_HttpRequest_API = `${environment.apiUrl}/api/performance/jmeter/http`;
 const JMeter_FtpRequest_API = `${environment.apiUrl}/api/performance/jmeter/ftp`;
 
@@ -31,10 +31,10 @@ export class PerformanceTestApiService {
     return response;
   }
 
-  getLatestReport(): Observable<string> {
-    // Appelle l'API pour récupérer le contenu du dernier rapport Gatling
-    return this.http.get(LATEST_REPORT_API, { responseType: 'text' });
-  }
+  // getLatestReport(): Observable<string> {
+  //   // Appelle l'API pour récupérer le contenu du dernier rapport Gatling
+  //   return this.http.get(LATEST_REPORT_API, { responseType: 'text' });
+  // }
   
   sendHttpJMeterRequest(
     jmeter_http_request: JMeterHttpRequest
